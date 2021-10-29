08ee8e00db0bfec6f36ec3913a66fe8cd4224045
skrBang <s_huabang@163.com>
2021-10-29 17:06:32 +0800

    add clip_grad_norm_

--- pd-coot/nntrainer/initializers.py
+++ pd-coot/nntrainer/initializers.py
@@ -6,6 +6,64 @@ import numpy as np
 from paddle import Tensor, concat, reshape
 import paddle
 
+from typext import INF as inf
+from typing import Union, Iterable
+
+_tensor_or_tensors = Union[paddle.Tensor, Iterable[paddle.Tensor]]
+
+paddle.set_device('gpu') if paddle.is_compiled_with_cuda() else paddle.set_device('cpu')
+
+
+def clip_grad_norm_(
+        parameters: _tensor_or_tensors, max_norm: float, norm_type: float = 2.0,
+        error_if_nonfinite: bool = False) -> paddle.Tensor:
+    r"""Clips gradient norm of an iterable of parameters.
+
+    The norm is computed over all gradients together, as if they were
+    concatenated into a single vector. Gradients are modified in-place.
+
+    Args:
+        parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
+            single Tensor that will have gradients normalized
+        max_norm (float or int): max norm of the gradients
+        norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
+            infinity norm.
+        error_if_nonfinite (bool): if True, an error is thrown if the total
+            norm of the gradients from :attr:``parameters`` is ``nan``,
+            ``inf``, or ``-inf``. Default: False (will switch to True in the future)
+
+    Returns:
+        Total norm of the parameters (viewed as a single vector).
+    """
+    if isinstance(parameters, paddle.Tensor):
+        parameters = [parameters]
+    parameters = [p for p in parameters if p.grad is not None]
+    max_norm = float(max_norm)
+    norm_type = float(norm_type)
+    if len(parameters) == 0:
+        return paddle.to_tensor(0.)
+    # device = paddle.get_device()  # parameters[0].grad.device
+    if norm_type == inf:
+        norms = [p.grad.detach().abs().max() for p in parameters]
+        total_norm = norms[0] if len(norms) == 1 else paddle.max(paddle.stack(norms))
+    else:
+        total_norm = paddle.norm(paddle.stack([paddle.norm(p.grad.detach(), norm_type) for p in parameters]), norm_type)
+    if error_if_nonfinite and paddle.logical_or(total_norm.isnan(), total_norm.isinf()):
+        raise RuntimeError(
+            f'The total norm of order {norm_type} for gradients from '
+            '`parameters` is non-finite, so it cannot be clipped. To disable '
+            'this error and scale the gradients by the non-finite norm anyway, '
+            'set `error_if_nonfinite=False`')
+    clip_coef = max_norm / (total_norm + 1e-6)
+    # Note: multiplying by the clamped coef is redundant when the coef is clamped to 1, but doing so
+    # avoids a `if clip_coef < 1:` conditional which can require a CPU <=> device synchronization
+    # when the gradients do not reside in CPU memory.
+    clip_coef_clamped = paddle.clip(clip_coef, max=1.0)
+    for p in parameters:
+        p.grad.detach().set_value(paddle.multiply(p.grad.detach(), clip_coef_clamped))
+        # p.grad.detach().mul_(clip_coef_clamped)
+    return total_norm
+
 
 def max(a: paddle.Tensor, axis=0, keepdim=True):
     """ndarray=numpy.array([[1, 2, 3, 4],
